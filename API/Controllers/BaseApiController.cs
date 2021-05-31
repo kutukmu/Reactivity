23d974d7b2f7c6e2fef3a090e750f321e40459b8
Kerim <kutukmu@gmail.com>
2021-05-31 07:45:02 -0500

    Crud operations

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,5 +1,6 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.DependencyInjection;
 namespace API.Controllers
 {
     [ApiController]
@@ -7,6 +8,7 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController:ControllerBase
     {
-        
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
